b7d698b846b591d8d8499894b15fb8a28ea7509b
Qixue Ju <P2617175@my365.dmu.ac.uk>
2021-03-23 17:39:33 +0800

    add clubController, Clubspage SignIn Models

---
+++ WebAppFootballClubs/WebAppFootballClubs/Models/ClubsPage.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppFootballClubs.Models
+{
+    public class ClubsPages
+    {
+        public string ClubName { get; set; }
+        public string ByLine { get; set; }
+        public string Content { get; set; }
+    }
+
+}
