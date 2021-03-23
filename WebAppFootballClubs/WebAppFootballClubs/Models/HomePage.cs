0b6acbe91fe1d5554eefaff479fba89e66dc77ca
Qixue Ju <P2617175@my365.dmu.ac.uk>
2021-03-23 16:29:00 +0800

    Add "SignInController"

---
+++ WebAppFootballClubs/WebAppFootballClubs/Models/HomePage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppFootballClubs.Models
+{
+    public class HomePage
+    {
+        public string Headline { get; set; }
+        public string ByLine { get; set; }
+        public string Content { get; set; }
+    }
+
+}
