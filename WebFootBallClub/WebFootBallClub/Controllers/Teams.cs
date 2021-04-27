573d6043e729426fed1d374a1a391dccb059ded7
Qixue Ju <P2617175@my365.dmu.ac.uk>
2021-04-28 00:00:56 +0800

    create team view & controller

---
+++ WebFootBallClub/WebFootBallClub/Controllers/Teams.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebFootBallClub.Models;
+
+namespace WebFootBallClub.Controllers
+{
+    public interface Teams
+    {
+        List<TeamsView> getAllTeams();
+    }
+}
