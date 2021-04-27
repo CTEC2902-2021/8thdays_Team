adf89244f18a59702f608e770941bf9df8770075
Boxuan Chen <P2617174@my365.dmu.ac.uk>
2021-04-28 02:37:49 +0800

    create players & teams view and modules, edit the view

---
+++ WebFootBallClub/WebFootBallClub/Models/TeamsView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFootBallClub.Models
+{
+    public class TeamsView
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MP { get; set; }
+        public int Win { get; set; }
+        public int Draw { get; set; }
+        public int Lose { get; set; }
+        public int GF { get; set; }
+        public int GA { get; set; }
+        public int GD { get; set; }
+        public int Pts { get; set; }
+    }
+}
