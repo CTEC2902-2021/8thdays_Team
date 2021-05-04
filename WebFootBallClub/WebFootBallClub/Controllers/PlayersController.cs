56a54d460ca6ea3f1ba10fa0331cbcf29230b693
Qixue Ju <P2617175@my365.dmu.ac.uk>
2021-05-04 22:39:57 +0800

    create player page

--- WebFootBallClub/WebFootBallClub/Controllers/PlayersController.cs
+++ WebFootBallClub/WebFootBallClub/Controllers/PlayersController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebFootBallClub.Models;
 
 namespace WebFootBallClub.Controllers
 {
     public class PlayersController : Controller
     {
-        // GET: Players
-        public ActionResult Index()
+        public List<Players> Team = new List<Players>();
+        public PlayersController()
         {
-            return View();
+            
+        }
+
+        private void LoanPlayerInformation()
+        {
+
         }
     }
 }
