ee0817e6f449b3e7e90da9a31aca1040a4c9178b
Qixue Ju <P2617175@my365.dmu.ac.uk>
2021-05-06 23:40:32 +0800

    finish the work

---
+++ WebFootBallClub/WebFootBallClub/Controllers/DataInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebFootBallClub.Models;
+
+namespace WebFootBallClub.Controllers
+{
+    public interface DataInterface
+    {
+        List<TeamsView> getAllTeams();
+        List<Player> getTeamPlayers(int TeamId);
+        string getTeamName(int TeamId);
+    }
+}
--- WebFootBallClub/WebFootBallClub/Controllers/HomeController.cs
+++ WebFootBallClub/WebFootBallClub/Controllers/HomeController.cs
@@ -3,26 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebFootBallClub.Controllers;
 
 namespace WebFootBallClub.Controllers
 {
     public class HomeController : Controller
     {
+        DataInterface TeamsView = new PremierLeagueController();
         public ActionResult Index()
         {
-            return View();
+            return View(TeamsView.getAllTeams());
+        }
+
+        public ActionResult Player (int ID)
+        {
+
+            return View(TeamsView.getTeamPlayers(ID));
+
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message1 = "Quick preview of Premier League";
+            ViewBag.Message2 = "Team:8thdays";
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message1 = "P2617174@my365.dmu.ac.uk";
+            ViewBag.Message2 = "P2617175@my365.dmu.ac.uk";
+            ViewBag.Message3 = "P2617176@my365.dmu.ac.uk";
+            ViewBag.Message4 = "P2617177@my365.dmu.ac.uk";
+            ViewBag.Message5 = "P2617178@my365.dmu.ac.uk";
 
             return View();
         }
